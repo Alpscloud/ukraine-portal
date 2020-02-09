f3e0162bd275ade42fa496effcd1db0e0eab6b3c
Serhii Kuptsov <winde100kg@gmail.com>
2020-02-09 16:55:40 +0200

    added a part of adaptive

--- src/js/common.js
+++ src/js/common.js
@@ -28,6 +28,24 @@ $(document).ready(function() {
 	});	
 	// ========= =========== =========== ===========
 
+	// ========== Scroll-to-top button ==========
+	$(window).on('scroll', function () {
+		if ($(this).scrollTop() > 150) {
+			$('.js-go-to-top-btn').fadeIn();
+		} else {
+			$('.js-go-to-top-btn').fadeOut();
+		}
+		
+	});
+
+	$('.js-go-to-top-btn').on('click', function (e) { 
+		e.preventDefault();
+		$('body, html').animate({
+			scrollTop: 0
+		}, 1000);
+	});
+	// ========= =========== =========== ===========
+
 
 	// Menu
 	$('.js-open-menu-btn').on('click', function(e) {
@@ -38,13 +56,24 @@ $(document).ready(function() {
 
 	// Sliders
 	var projectsSlider = new Swiper('.js-projects-slider', {
-		slidesPerView: 3,
-		spaceBetween: 50,
+		slidesPerView: 1,
+		spaceBetween: 10,
+		loop: true,
 		navigation: {
 			clickable: true,
 			nextEl: '.js-projects-slider-btn-next',
     	prevEl: '.js-projects-slider-btn-prev',
 		},
+		breakpoints: {
+			1100: {
+				slidesPerView: 3,
+				spaceBetween: 50
+			},
+			640: {
+				slidesPerView: 2,
+				spaceBetween: 20
+			}
+		}
 	});
 
 
